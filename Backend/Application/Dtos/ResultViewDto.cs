3e6ddbdc0eb4080c94a3e6ed809bf82f4ef72831
Матвей))))))) <77619364+russkoepoleexperimentov@users.noreply.github.com>
2026-08-15 04:00:15 +0700

    DTOs

---
+++ Backend/Application/Dtos/ResultViewDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Dtos
+{
+    public class ResultViewDto
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; } = null!;
+        public int DeltaSeconds { get; set; }
+        public DateTime FirstExcecutionTime { get; set; }
+        public int AverageExcecutionTime { get; set; }
+        public float AverageValue { get; set; }
+        public float MedianValue { get; set; }
+        public float MinimumValue { get; set; }
+        public float MaximumValue { get; set; }
+    }
+}
---
+++ Backend/Application/Dtos/ValueViewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos
+{
+    public class ValueViewDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public int ExceutionTime { get; set; }
+        public float Value { get; set; }
+    }
+}
