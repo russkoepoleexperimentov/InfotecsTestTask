578d0c0b526774becae9b0971d67ce01550845e4
Матвей))))))) <77619364+russkoepoleexperimentov@users.noreply.github.com>
2026-08-15 03:56:36 +0700

    Domain entities

---
+++ Backend/Domain/Entities/Value.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class ValueRecord // таблица values
+    {
+        public DateTime Date { get; set; }
+        public int ExceutionTime {  get; set; }
+        public float Value { get; set; }
+    }
+}
