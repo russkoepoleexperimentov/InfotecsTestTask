555e075eda9ca7aa55987965f120b0af2d8269f3
Матвей))))))) <77619364+russkoepoleexperimentov@users.noreply.github.com>
2026-08-15 04:12:15 +0700

    PagedList dto

---
+++ Backend/Application/Dtos/PagedListDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos
+{
+    public class PagedListDto<T>
+    {
+        public List<T> Values { get; set; } = null!;
+        public int Skipped { get; set; }
+        public int Taken { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
