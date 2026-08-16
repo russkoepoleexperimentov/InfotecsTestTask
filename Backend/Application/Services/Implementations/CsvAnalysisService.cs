d6d76f0e46220ce6175e06013f79377bf9025e1a
Матвей))))))) <77619364+russkoepoleexperimentov@users.noreply.github.com>
2026-08-17 02:43:17 +0700

    File parsing fix

--- Backend/Application/Services/Implementations/CsvAnalysisService.cs
+++ Backend/Application/Services/Implementations/CsvAnalysisService.cs
@@ -8,14 +8,18 @@ namespace Application.Services.Implementations
 {
     public class CsvAnalysisService : ICsvAnalysisService
     {
-        const char CsvDelimeter = ',';
+        const char CsvDelimeter = ';';
 
 
         private readonly string _csvHeader;
+        private readonly DateTime _minDate;
+        private readonly int _linesLimit;
 
         public CsvAnalysisService() 
         {
             _csvHeader = string.Join(CsvDelimeter, ["Date", "ExecutionTime", "Value"]);
+            _minDate = new DateTime(2000, 01, 01, 0, 0, 0, DateTimeKind.Utc);
+            _linesLimit = 10000;
         }
 
         public async Task<Result<List<ValueRecord>>> UploadCsv(string fileName, string fileContentType, Stream fileReadStream, long fileLength)
@@ -26,55 +30,62 @@ namespace Application.Services.Implementations
             if (fileReadStream is null)
                 return Result.Fail("Error while reading a file");
 
-            var reader = new StreamReader(fileReadStream);
+            using var reader = new StreamReader(fileReadStream);
 
             var header = await reader.ReadLineAsync();
 
-            if(header is null || header == _csvHeader)
+            if(header is null || header != _csvHeader)
                 return Result.Fail("Wrong CSV header");
 
             List<ValueRecord> testOutput = new List<ValueRecord>();
 
-            int lineNum = 1;
-            while (!reader.EndOfStream) 
-            {
-                var line = await reader.ReadLineAsync();
+            int lineNum = 1; // at header on start
 
+            while (await reader.ReadLineAsync() is { } line)
+            {
                 if (string.IsNullOrWhiteSpace(line))
                     continue;
 
+                lineNum++;
+
+                if (lineNum > _linesLimit + 1)
+                    return Result.Fail($"Invalid lines count (max: {_linesLimit})");
+
                 var tokens = line.Split(CsvDelimeter).Select(token => token.Trim()).ToArray();
 
-                if (tokens == null || tokens.Length == 0)
-                    continue;
+                if (tokens.Length != 3)
+                    return Result.Fail($"Invalid tokens length @ {lineNum}");
 
                 var valueRecord = new ValueRecord();
 
-
-                if (!DateTime.TryParse(tokens[0], null, DateTimeStyles.RoundtripKind, out DateTime date))
-                {
+                if (!DateTime.TryParseExact(tokens[0], "yyyy-MM-ddTHH:mm:ss.ffffZ", CultureInfo.InvariantCulture, 
+                    DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal, out var date))
                     return Result.Fail($"Failed parse a {nameof(date)} @ {lineNum}");
-                }
 
-                if(!int.TryParse(tokens[1], out int executionTime))
-                {
+                if (!int.TryParse(tokens[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out int executionTime))
                     return Result.Fail($"Failed parse an {nameof(executionTime)} @ {lineNum}");
-                }
 
-                if (!float.TryParse(tokens[2], out float value))
-                {
+                if (!float.TryParse(tokens[2], NumberStyles.Float, CultureInfo.InvariantCulture, out float value))
                     return Result.Fail($"Failed parse an {nameof(value)} @ {lineNum}");
-                }
 
+                if(date < _minDate || date > DateTime.UtcNow)
+                    return Result.Fail($"Invalid date @ {lineNum}");
+
+                if (executionTime < 0)
+                    return Result.Fail($"Invalid executionTime @ {lineNum}");
+
+                if (value < 0)
+                    return Result.Fail($"Invalid value @ {lineNum}");
 
                 valueRecord.Date = date;
                 valueRecord.Value = value;
                 valueRecord.ExceutionTime = executionTime;
 
                 testOutput.Add(valueRecord);
-
-                lineNum++;
             }
+
+            if (lineNum < 2)
+                return Result.Fail($"Invalid lines count (min: 1)");
 
             return Result.Ok(testOutput);
         }
